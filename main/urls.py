5d6a9b3e43531302eca18b334a3a6c5d22ff973e
Vasanth <vasanthkganesan@gmail.com>
2023-12-10 22:30:48 +0000

    Removed django

--- main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView
-from . import views
-
-urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('contact/', ContactPageView.as_view(), name='contact'),
-]
---
+++ scripts/base.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  var phoneInput = document.getElementById('phone');
+  phoneInput.oninvalid = function(event) {
+      event.target.setCustomValidity('Please enter a valid phone number.');
+  }
+  phoneInput.oninput = function(event) {
+      event.target.setCustomValidity('');
+  }
+}
+const form = document.getElementById('ContactForm');
+
+form.addEventListener('submit', function(e) {
+
+  const hCaptcha = form.querySelector('textarea[name=h-captcha-response]').value;
+
+  if (!hCaptcha) {
+      e.preventDefault();
+      alert("Please fill out captcha field")
+      return
+  }
+});
